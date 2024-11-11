f2136e837b61fb9b719a026d5a6d6c5621d54e2a
taehhh8 <95203882+taehhh8@users.noreply.github.com>
2024-11-11 10:48:28 +0900

    nextjs  modify
    nextjs  modify

---
+++ learn-nextjs14/app/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return <h1>About us!</h1>;
+}
+
+// ----> <Home/>
+// about-us ----> <About/>
+// movies/:id ----> <Movie/>
---
+++ learn-nextjs14/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return <h1>Hello NextJs</h1>;
+}
+
+// ----> <Home/>
+// about-us ----> <About/>
+// movies/:id ----> <Movie/>
